6cdceefd66ed58096b3ef1a775f6de91d6f7ebe9
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-06-28 02:38:49 -0600

    problem-solved

---
+++ 4_invertedWord.js
@@ -0,0 +1,35 @@
+/* 
+
+  * EXERCISE 4
+* Given a word as parameter, invert it.
+* Create a function which inverts the word
+* You cannot use any js properties.
+* You can oly use control structures
+
+* Examples:
+* 'hi' => ih
+* 'carlos' => solrac
+
+*/
+
+const text = 'carlos'
+
+function invertedWord(text) {
+
+  // variable created as empty string
+  let inverted = ''
+  /* 
+
+  for (let letter of text) {
+    inverted = `${letter}${inverted}`
+  }
+  return `${text} is ${inverted}` */
+
+  //for (i starts at the last character of the word; i has to be greater o equals to 0; i decrements)
+  for (let i = text.length - 1; i >= 0; i--) {
+    inverted += text[i]
+  }
+  return inverted
+}
+
+console.log(invertedWord(text))
