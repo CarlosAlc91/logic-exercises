2b5024ab99e4798fd97c14235aa6da8ee6eaa2e8
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-06-29 03:03:16 -0600

    exercise-completed

--- 5_percentage.js
+++ 5_percentage.js
@@ -7,3 +7,18 @@
   * percentage(43, 897) //returns 385.71  
 
 */
+
+const percentage = 20
+const number = 100
+
+function percentageOf(percentage, number) {
+
+  let computing = ((number / 100) * percentage)
+  let result = `${percentage}% of ${number} is ${computing}`
+
+  /* return `${percentage}% of ${number} is ${computing}` */
+
+  return result
+}
+
+console.log(percentageOf(percentage, number))
